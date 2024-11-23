90bdfb05e9f00c781eee8ef98213cc42c87670d4
MateuszIw <mateusziwanek@hotmail.com>
2024-11-23 15:47:49 +0100

    *MovementInput(EnhancedInput) *PlayerController

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,42 @@
+// Copyright  Mateusz Iwanek
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AAuraPlayerController();
+
+
+protected:
+	virtual void BeginPlay() override;
+
+
+	virtual void SetupInputComponent() override;
+	//insted of 
+	//virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override; //.. in character class
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
+};
