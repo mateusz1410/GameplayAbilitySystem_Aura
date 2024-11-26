2a23ab6bf697510239156ad413d084a61b1e553b
MateuszIw <mateusziwanek@hotmail.com>
2024-11-26 19:45:58 +0100

    * GE  health and mana * bp crystal and potion

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -10,7 +10,7 @@ UAuraAttributeSet::UAuraAttributeSet()
 	InitHealth(50.f);// accesor InitParamName, too early to call SETTER in construct, ony init
 	InitMaxHealth(100.f);
 
-	InitMana(50.f);
+	InitMana(25.f);
 	InitMaxMana(50.f);
 }
 
--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -2,53 +2,36 @@
 
 
 #include "Actor/AuraEffectActor.h"
-#include "AbilitySystemInterface.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystem/AuraAttributeSet.h"
-#include "Components/SphereComponent.h"
-
+#include "AbilitySystemBlueprintLibrary.h"
 
 AAuraEffectActor::AAuraEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
-	
-}
-
-void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResault)
-{
-	//TEST ONLY
-	// 
-	//TODO: Change this to apply a Gameplay Effect. ussing const_cast as hack! is bad
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
-		check(AuraAttributeSet); // every actor impplent ASCInterface gots UAuraAttributeSet in this case
-		
-		
-		UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);// cast to remove const
-		MutableAuraAttributeSet->SetHealth(AuraAttributeSet->GetHealth() + 25.f); // const shoudn't be set
-		MutableAuraAttributeSet->SetMana(AuraAttributeSet->GetMana() - 25.f); // const shoudn't be set
-		Destroy();
-	}
-}
-
-void AAuraEffectActor::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
 void AAuraEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
+}
 
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::OnEndOverlap);
+void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor); // better use this insted of cast
+	
+	if (TargetASC == nullptr) return;
+
+	checkf(GameplayEffectClass,TEXT("GameplayEffectClass class is null, please set it in EffectActor"))
+
+	FGameplayEffectContextHandle EffectContextHandle =  TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this); // what cose effect
+
+    const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); // pointer value 
+	 // EffectSpecHandle -get- FGameplayEffectSpecHandle ---> .Data -get- TSherePtr<FGameplayEffectSpec> --->.Get() - get- *TSherePtr<FGameplayEffectSpec> ---> *  -get -  ref &
 	
 }
 
